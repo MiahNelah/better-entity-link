4b72723cd3e85062389cb8d781d30346071ca4b0
Miah Nelah <miahnelah@proton.me>
2023-09-11 08:29:00 +0200

    replace all references of "entity" by "document" to fit Foundry's standard

---
+++ module/src/BetterDocumentLink.js
@@ -0,0 +1,149 @@
+export class BetterDocumentLink {
+    static get id() {
+        return "better-entity-link"
+    }
+
+    static _contextMenuName() {
+        return "BetterDocumentLink"
+    }
+
+    static instance() {
+        return game.modules.get(BetterDocumentLink.id)?._instance
+    }
+
+    static registerAction(documentType, options) {
+        const instance = BetterDocumentLink.instance();
+        if (instance === undefined) {
+            BetterDocumentLink.initialize()
+            return BetterDocumentLink.registerAction(documentType, options);
+        }
+        return instance.registerAction(documentType, options)
+    }
+
+    static enhanceDocumentLinks(app, html, data) {
+        return BetterDocumentLink.instance().enhanceDocumentLinks(app, html, data)
+    }
+
+    static registerActorAction(options) {
+        return BetterDocumentLink.registerAction("Actor", options);
+    }
+
+    static registerItemAction(options) {
+        return BetterDocumentLink.registerAction("Item", options);
+    }
+
+    static registerSceneAction(options) {
+        return BetterDocumentLink.registerAction("Scene", options);
+    }
+
+    static registerJournalEntryAction(options) {
+        return BetterDocumentLink.registerAction("JournalEntry", options);
+    }
+
+    static registerJournalEntryPageAction(options) {
+        return BetterDocumentLink.registerAction("JournalEntryPage", options);
+    }
+
+    static registerMacroAction(options) {
+        return BetterDocumentLink.registerAction("Macro", options);
+    }
+
+    static registerRolltableAction(options) {
+        return BetterDocumentLink.registerAction("RollTable", options);
+    }
+
+    static registerCardStacksAction(options) {
+        return BetterDocumentLink.registerAction("Cards", options);
+    }
+
+    static registerPlaylistAction(options) {
+        return BetterDocumentLink.registerAction("Playlist", options);
+    }
+
+    static _defaultContextMenu() {
+        return {
+            "Actor": [],
+            "Item": [],
+            "Scene": [],
+            "JournalEntry": [],
+            "JournalEntryPage": [],
+            "Macro": [],
+            "RollTable": [],
+            "Cards": [],
+            "Playlist": []
+        };
+    }
+
+    static initialize() {
+        if (BetterDocumentLink.instance() !== undefined) return;
+
+        const module = game.modules.get(BetterDocumentLink.id);
+        const instance = new BetterDocumentLink(module);
+        module.registerAction =  instance.registerAction;
+        module.enhanceDocumentLink = instance.enhanceDocumentLink;
+        module.enhanceDocumentLinks = instance.enhanceDocumentLinks;
+
+        module.registerActorAction = BetterDocumentLink.registerActorAction;
+        module.registerItemAction = BetterDocumentLink.registerItemAction;
+        module.registerSceneAction = BetterDocumentLink.registerSceneAction;
+        module.registerJournalEntryAction = BetterDocumentLink.registerJournalEntryAction;
+        module.registerJournalEntryPageAction = BetterDocumentLink.registerJournalEntryPageAction;
+        module.registerMacroAction = BetterDocumentLink.registerMacroAction;
+        module.registerRolltableAction = BetterDocumentLink.registerRolltableAction;
+        module.registerCardStacksAction = BetterDocumentLink.registerCardStacksAction;
+        module.registerPlaylistAction = BetterDocumentLink.registerPlaylistAction;
+    }
+
+    constructor(module) {
+        this.contextMenus = duplicate(BetterDocumentLink._defaultContextMenu());
+        module._instance = this;
+    }
+
+    registerAction(documentType, options) {
+        const actionMenu = {
+            name: options.name,
+            icon: `<i class="fas ${options.icon}"></i>`,
+            condition: li => {
+                const documentUuid = li.data("uuid");                
+                const document = fromUuidSync(documentUuid);                
+                return options.condition instanceof Function && options.condition(documentUuid, document);
+            },
+            callback: async li => {
+                const documentUuid = li.data("uuid");                
+                const document = await fromUuid(documentUuid);
+                return await options.callback(document);
+            }
+        }
+        this.contextMenus[documentType].push(actionMenu);
+    }
+
+    enhanceDocumentLink(app, link) {
+        const documentType = this._resolveDocumentType($(link));
+        const contextOptions = this.contextMenus[documentType];
+        if (!contextOptions?.length) return undefined;
+        ContextMenu.create(app, $(link), "a.content-link", contextOptions, BetterDocumentLink._contextMenuName);
+    }
+
+    enhanceDocumentLinks(app, html) {
+        if (!html === null || !html) return undefined;
+
+        const links = html.find("a.content-link:not([data-contextmenu])");
+        if (links === null || links === undefined || (Array.isArray(links) && !links.length)) return undefined;
+
+        setTimeout(() => {
+            for (let link of links) {
+                this.enhanceDocumentLink(app, $(link));
+            }
+        }, 100);
+    }
+
+    _resolveDocumentType(documentLink) {
+        if (documentLink[0].hasAttribute("data-type")) return documentLink.data("type");
+        if (documentLink[0].hasAttribute("data-pack")) {
+            const packId = documentLink.data("pack");
+            return game.packs.get(packId).documentName;
+        }
+        // TODO: add resolving from <i> font-awesome icon class ?
+        return undefined;
+    }
+}
--- module/src/better-entity-link.js
+++ module/src/better-entity-link.js
@@ -1,4 +1,4 @@
-import {BetterEntityLink} from "./BetterEntityLink.js";
+import {BetterDocumentLink} from "./BetterDocumentLink.js";
 
 function renderImagePopup(image, title, uuid, shareable) {
     const imagePoput = new ImagePopout(image, {
@@ -44,164 +44,164 @@ function safeSetInterval(condition, callback, delay, timeout) {
 
 function registerActorActions() {
     // Actor - "Select" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "CONTROLS.CanvasSelect",
         icon: "fa-hand-pointer",
         condition: (uuid) => canvas.ready && canvas.tokens.placeables.some(x => x.actor.uuid.endsWith(uuid)),
-        callback: async entity => {
+        callback: async document => {
             if (!canvas.tokens.active) canvas.tokens.activate();
             canvas.tokens.releaseAll();
-            entity.getActiveTokens().forEach(x => x.control({releaseOthers: false}));
+            document.getActiveTokens().forEach(x => x.control({releaseOthers: false}));
         }
     });
 
     // Actor - "Target" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "CONTROLS.TargetSelect",
         icon: "fa-bullseye",
         condition: (uuid) => canvas.ready && canvas.tokens.placeables.some(x => x.actor.uuid.endsWith(uuid)),
-        callback: async entity => {
+        callback: async document => {
             if (!canvas.tokens.active) canvas.tokens.activate();
-            entity.getActiveTokens()
+            document.getActiveTokens()
                 .filter(token => !token.isTargeted)
                 .forEach(token => token.setTarget(true, {releaseOthers: false}));
         }
     });
 
     // Actor - "Ping" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "CONTROLS.CanvasPing",
         icon: "fa-map-pin",
         condition: (uuid) => canvas.ready && canvas.tokens.placeables.some(x => x.actor.uuid.endsWith(uuid)),
-        callback: async entity => Promise.all(entity.getActiveTokens().map(async x => canvas.ping(x.center)))
+        callback: async document => Promise.all(document.getActiveTokens().map(async x => canvas.ping(x.center)))
     });
 
     // Actor - "Ping Alert" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "CONTROLS.CanvasPingAlert",
         icon: "fa-map-pin",
         condition: (uuid) => canvas.ready && canvas.tokens.placeables.some(x => x.actor.uuid.endsWith(uuid)),
-        callback: async entity => Promise.all(entity.getActiveTokens().map(async x => canvas.ping(x.center, {style: CONFIG.Canvas.pings.types.ALERT})))
+        callback: async document => Promise.all(document.getActiveTokens().map(async x => canvas.ping(x.center, {style: CONFIG.Canvas.pings.types.ALERT})))
     });
 
     // Actor - "Pull to Ping" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "CONTROLS.CanvasPingPull",
         icon: "fa-map-pin",
         condition: (uuid) => canvas.ready && canvas.tokens.placeables.some(x => x.actor.uuid.endsWith(uuid)),
-        callback: async entity => Promise.all(entity.getActiveTokens().map(async x => canvas.ping(x.center, {
+        callback: async document => Promise.all(document.getActiveTokens().map(async x => canvas.ping(x.center, {
             style: CONFIG.Canvas.pings.types.PULL,
             pull: true
         })))
     });
 
     // Actor - "Token Configuration" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "TOKEN.Title",
         icon: "fa-user-circle",
         condition: (uuid, data) => data?.prototypeToken && (game.user.isGM || game.user.isTrusted || permissionHelper(data, [CONST.ENTITY_PERMISSIONS.OBSERVER, CONST.ENTITY_PERMISSIONS.OWNER])),
-        callback: async entity => new CONFIG.Token.prototypeSheetClass(entity.prototypeToken).render(true)
+        callback: async document => new CONFIG.Token.prototypeSheetClass(document.prototypeToken).render(true)
     });
 
     // Actor - "View Character Artwork" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "SIDEBAR.CharArt",
         icon: "fa-image",
         condition: (uuid, data) => data?.img && ![undefined, null, CONST.DEFAULT_TOKEN].includes(data?.img),
-        callback: async entity => renderImagePopup(entity.img, entity.name, entity.uuid, true)
+        callback: async document => renderImagePopup(document.img, document.name, document.uuid, true)
     });
 
     // Actor - "View Token Artwork" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "SIDEBAR.TokenArt",
         icon: "fa-image",
         condition: (uuid, data) => data?.prototypeToken?.texture?.src && ![undefined, null, CONST.DEFAULT_TOKEN].includes(data?.prototypeToken.texture.src),
-        callback: async entity => renderImagePopup(entity.prototypeToken.texture.src, entity.name, entity.uuid, true)
+        callback: async document => renderImagePopup(document.prototypeToken.texture.src, document.name, document.uuid, true)
     });
 
     // Actor - "Configure Ownership" button
-    BetterEntityLink.registerActorAction({
+    BetterDocumentLink.registerActorAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
 function registerItemActions() {
     // Item - "View Item Artwork" button
-    BetterEntityLink.registerItemAction({
+    BetterDocumentLink.registerItemAction({
         name: "ITEM.ViewArt",
         icon: "fa-image",
         condition: (uuid, data) => ![undefined, null, CONST.DEFAULT_TOKEN].includes(data?.img),
-        callback: async entity => renderImagePopup(entity.img, entity.name, entity.uuid, true)
+        callback: async document => renderImagePopup(document.img, document.name, document.uuid, true)
     });
 
     // Item - "View Item Artwork" button
-    BetterEntityLink.registerItemAction({
+    BetterDocumentLink.registerItemAction({
         name: "OWNERSHIP.OWNER",
         icon: "fa-crown",
         condition: (uuid, data) => data && data.isEmbedded && data.parent,
-        callback: async entity => entity.parent.sheet.render(true)
+        callback: async document => document.parent.sheet.render(true)
     });
 
     // Item - "Configure Ownership" button
-    BetterEntityLink.registerItemAction({
+    BetterDocumentLink.registerItemAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
 function registerSceneActions() {
     // Scene - "View" button
-    BetterEntityLink.registerSceneAction({
+    BetterDocumentLink.registerSceneAction({
         name: "SCENES.View",
         icon: "fa-eye",
         condition: (uuid, data) => (game.user.isGM || game.user.isTrusted) && !data.isView,
-        callback: async entity => await entity.view()
+        callback: async document => await document.view()
     });
 
     // Scene - "Activate" button
-    BetterEntityLink.registerSceneAction({
+    BetterDocumentLink.registerSceneAction({
         name: "SCENES.Activate",
         icon: "fa-bullseye",
         condition: (uuid, data) => (game.user.isGM || game.user.isTrusted) && !data.active,
-        callback: async entity => await entity.activate()
+        callback: async document => await document.activate()
     });
 }
 
 function registerJournalEntryActions() {
     // JournalEntry - "Show players (Text)" button
-    BetterEntityLink.registerJournalEntryAction({
+    BetterDocumentLink.registerJournalEntryAction({
         name: `${game.i18n.localize("JOURNAL.ActionShow")} (${game.i18n.localize("JOURNAL.ModeText")})`,
         icon: "fa-eye",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted),
-        callback: async entity => await showImage(entity.uuid, entity.name, "text")
+        callback: async document => await showImage(document.uuid, document.name, "text")
     });
 
     // JournalEntry - "Show players (Image)" button
-    BetterEntityLink.registerJournalEntryAction({
+    BetterDocumentLink.registerJournalEntryAction({
         name: `${game.i18n.localize("JOURNAL.ActionShow")} (${game.i18n.localize("JOURNAL.ModeImage")})`,
         icon: "fa-eye",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted),
-        callback: async entity => await showImage(entity.uuid, entity.name, "image")
+        callback: async document => await showImage(document.uuid, document.name, "image")
     });
 
     // JournalEntry - "Jump to Pin" button
     // If no pin is found in current scene, look up for pins in others scenes
-    BetterEntityLink.registerJournalEntryAction({
+    BetterDocumentLink.registerJournalEntryAction({
         name: "SIDEBAR.JumpPin",
         icon: "fa-crosshairs",
         condition: (uuid, data) => data.sceneNote ? true
             : game.scenes.filter(s => s.notes.filter(x => x.entryId === data.id).length > 0).length > 0,
-        callback: async entity => {
-            if (entity.scenenote)
-                return entity.panToNote();
+        callback: async document => {
+            if (document.scenenote)
+                return document.panToNote();
 
             // Note is not in current scene, we look in all scenes and take first match
-            const scene = game.scenes.filter(s => s.notes.filter(x => x.entryId === entity.id).length > 0)[0];
+            const scene = game.scenes.filter(s => s.notes.filter(x => x.entryId === document.id).length > 0)[0];
             await scene.view();
 
             // Wait 30s maximum for canvas to be ready before panning to note. Check is done every 0.5 second.
@@ -210,115 +210,115 @@ function registerJournalEntryActions() {
     });
 
     // JournalEntry - "Configure Ownership" button
-    BetterEntityLink.registerJournalEntryAction({
+    BetterDocumentLink.registerJournalEntryAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
 function registerJournalEntryPageActions() {
     // JournalEntryPage - "Show players (Text)" button
-    BetterEntityLink.registerJournalEntryPageAction({
+    BetterDocumentLink.registerJournalEntryPageAction({
         name: `${game.i18n.localize("JOURNAL.ActionShow")} (${game.i18n.localize("JOURNAL.ModeText")})`,
         icon: "fa-eye",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted),
-        callback: async entity => await showImage(entity.uuid, entity.name, "text")
+        callback: async document => await showImage(document.uuid, document.name, "text")
     });
 
     // JournalEntryPage - "Show players (Image)" button
-    BetterEntityLink.registerJournalEntryPageAction({
+    BetterDocumentLink.registerJournalEntryPageAction({
         name: `${game.i18n.localize("JOURNAL.ActionShow")} (${game.i18n.localize("JOURNAL.ModeImage")})`,
         icon: "fa-eye",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted),
-        callback: async entity => await showImage(entity.uuid, entity.name, "image")
+        callback: async document => await showImage(document.uuid, document.name, "image")
     });
 
     // JournalEntryPage - "Configure Ownership" button
-    BetterEntityLink.registerJournalEntryPageAction({
+    BetterDocumentLink.registerJournalEntryPageAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
 function registerMacroActions() {
     // Macro - "Edit Macro" button
-    BetterEntityLink.registerMacroAction({
+    BetterDocumentLink.registerMacroAction({
         name: "MACRO.Edit",
         icon: "fa-edit",
         condition: (uuid, data) => game.user.isGM || game.user.isTrusted || permissionHelper(data, [CONST.ENTITY_PERMISSIONS.OBSERVER, CONST.ENTITY_PERMISSIONS.OWNER]),
-        callback: async entity => entity.sheet.render(true)
+        callback: async document => document.sheet.render(true)
     });
 }
 
 function registerRollTableActions() {
     // RollTable - "Roll" button
-    BetterEntityLink.registerRolltableAction({
+    BetterDocumentLink.registerRolltableAction({
         name: "TABLE.Roll",
         icon: "fa-dice-d20",
         condition: (uuid, data) => (game.user.isGM || game.user.isTrusted) || permissionHelper(data, [CONST.ENTITY_PERMISSIONS.OBSERVER, CONST.ENTITY_PERMISSIONS.OWNER]),
-        callback: async entity => await entity.draw()
+        callback: async document => await document.draw()
     });
 
     // RollTable - "Configure Ownership" button
-    BetterEntityLink.registerRolltableAction({
+    BetterDocumentLink.registerRolltableAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
 function registerCardsActions() {
     // Cardstacks - "Shuffle" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "CARDS.Shuffle",
         icon: "fa-random",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted) && data.type.localeCompare("hand", undefined, {sensitivity: "base"}) !== 0,
-        callback: async entity => await entity.shuffle()
+        callback: async document => await document.shuffle()
     });
 
     // Cardstacks - "Draw" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "CARDS.Draw",
         icon: "fa-edit",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted) && data.type.localeCompare("hand", undefined, {sensitivity: "base"}) === 0,
-        callback: async entity => await entity.drawDialog()
+        callback: async document => await document.drawDialog()
     });
 
     // Cardstacks - "Deal" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "CARDS.Deal",
         icon: "fa-share-square",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted) && data.type.localeCompare("deck", undefined, {sensitivity: "base"}) === 0,
-        callback: async entity => await entity.dealDialog()
+        callback: async document => await document.dealDialog()
     });
 
     // Cardstacks - "Pass" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "CARDS.Pass",
         icon: "fa-share-square",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted) && data.type.localeCompare("deck", undefined, {sensitivity: "base"}) !== 0,
-        callback: async entity => await entity.passDialog()
+        callback: async document => await document.passDialog()
     });
 
     // Cardstacks - "Reset" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "CARDS.Reset",
         icon: "fa-undo",
         condition: (uuid, data) => data?.img !== CONST.DEFAULT_TOKEN && (game.user.isGM || game.user.isTrusted),
-        callback: async entity => await entity.resetDialog()
+        callback: async document => await document.resetDialog()
     });
 
     // Cardstacks - "Configure Ownership" button
-    BetterEntityLink.registerCardStacksAction({
+    BetterDocumentLink.registerCardStacksAction({
         name: "OWNERSHIP.Configure",
         icon: "fa-lock fa-fw",
         condition: () => game.user.isGM || game.user.isTrusted,
-        callback: async entity => new DocumentOwnershipConfig(entity).render(true)
+        callback: async document => new DocumentOwnershipConfig(document).render(true)
     });
 }
 
@@ -337,10 +337,10 @@ Hooks.on('ready', () => {
     registerCardsActions();
     registerPlaylistActions();
 
-    Hooks.on('renderJournalSheet', BetterEntityLink.enhanceEntityLinks);
-    Hooks.on('renderActorSheet', BetterEntityLink.enhanceEntityLinks);
-    Hooks.on('renderJournalPageSheet', BetterEntityLink.enhanceEntityLinks);
-    Hooks.on('renderItemSheet', BetterEntityLink.enhanceEntityLinks);
+    Hooks.on('renderJournalSheet', BetterDocumentLink.enhanceDocumentLinks);
+    Hooks.on('renderActorSheet', BetterDocumentLink.enhanceDocumentLinks);
+    Hooks.on('renderJournalPageSheet', BetterDocumentLink.enhanceDocumentLinks);
+    Hooks.on('renderItemSheet', BetterDocumentLink.enhanceDocumentLinks);
     //Hooks.on('renderChatMessage', BetterEntityLink.enhanceEntityLinks);
 })
 
